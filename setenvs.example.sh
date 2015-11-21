ce799c04ce9a3ce638a75462f67bb3b50295f737
John Muth <johnmuthuk@gmail.com>
2015-11-21 14:02:15 +0000

    added comments

--- setenvs.example.sh
+++ setenvs.example.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+# All of the following details should come from an 'app' that you have created
+# using the Twitter account that you want this bot to use to tweet.
+# https://apps.twitter.com/app/new
+
 export TWITTER_CONSUMER_KEY=''
 export TWITTER_CONSUMER_SECRET=''
 export TWITTER_ACCESS_TOKEN=''
 export TWITTER_ACCESS_TOKEN_SECRET=''
 
+# If 2 data dirs are defined, the bot will use both, alternating between them.
 export DATA_DIR_1=''
 #export DATA_DIR_2=''
 
