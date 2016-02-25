ee35733d9c39bcda17b22c1c8c1125d7a7215840
John Muth <johnmuthuk@gmail.com>
2016-02-25 09:39:19 +0000

    Update setenvs.example.sh

--- setenvs.example.sh
+++ setenvs.example.sh
@@ -4,15 +4,10 @@
 # using the Twitter account that you want this bot to use to tweet.
 # https://apps.twitter.com/app/new
 
-
-export TWITTER_CONSUMER_KEY='mudWaPDVNZuFuUeGI5CS9SACO'
-export TWITTER_CONSUMER_SECRET='qMFNYM6uCMtkBhcuC3nxnpteCHMFJMj7HcIeUwJWaNms2BAMwy'
-export TWITTER_ACCESS_TOKEN='4241112023-gMhEliXG0XS1689LQ7enkNgFXfNZz46cCyN2Z59'
-export TWITTER_ACCESS_TOKEN_SECRET='4Nfc8fXgNKr2nvHbTsv27yXpIMuBgBRyHxgwTare4WRFq'
-
+export TWITTER_CONSUMER_KEY='consumer_key'
+export TWITTER_CONSUMER_SECRET='consumer_secret'
+export TWITTER_ACCESS_TOKEN='access_token'
+export TWITTER_ACCESS_TOKEN_SECRET='access_token_secret'
 export DATA_DIRS='["/Users/johnmuth/experiments/movie-quote-tweet-bot/movie-quotes/Scarface","/Users/johnmuth/experiments/movie-quote-tweet-bot/movie-quotes/Willy+Wonka+%26+the+Chocolate+Factory"]'
-
 export DRY_RUN=true
-
-#export TWEET_INTERVAL_MINUTES=1
-export TWEET_INTERVAL_SECONDS=.5
+export TWEET_INTERVAL_MINUTES=60
